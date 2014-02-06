155e1b5854168cf3008d84dd7472f5e3ec85c629
Hector Gil <gilmh93@gmail.com>
2014-02-06 19:37:05 +0100

    practica VLCJ 2

---
+++ ReproductorHector.2/src/org/hectordam/reproductorHector/Ventana.java
@@ -0,0 +1,423 @@
+package org.hectordam.reproductorHector;
+
+import java.awt.EventQueue;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.mrl.CdMrl;
+import uk.co.caprica.vlcj.mrl.SimpleDvdMrl;
+import uk.co.caprica.vlcj.player.embedded.DefaultFullScreenStrategy;
+import uk.co.caprica.vlcj.player.embedded.FullScreenStrategy;
+import uk.co.caprica.vlcj.runtime.RuntimeUtil;
+
+import java.awt.BorderLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.JSlider;
+import javax.swing.JInternalFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.JLabel;
+
+import org.hectordam.reproductorHector.base.Reproduccion;
+
+import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.awt.Toolkit;
+
+public class Ventana implements FullScreenStrategy{
+
+	public JFrame frame;
+	private JMenuItem mntmAbrir;
+	private JMenuItem mntmAbrirDvd;
+	private JMenuItem mntmAbrirCds;
+	private JMenuItem mntmTamano;
+	private JMenuItem mntmSubtitulos;
+	private JMenuItem mntmListaDeReproduccion;
+	private JMenuItem mntmRecientes;
+	private JMenuItem mntmCaptura;
+	private JMenuItem menuItem;
+	private JMenuItem menuItem_1;
+	private JMenuItem mntmPantallaCompleta;
+	private JMenuItem mntmPredeterminada;
+	
+	public JButton btRetrasar;
+	public JButton btPlay;
+	public JButton btPause;
+	public JButton btStop;
+	public JButton btAdelantar;
+	public JSlider slVolumen;
+	public JSlider slTiempo;
+	private static final String ruta_libreria = "C:\\Users\\nose\\Desktop\\2-DAM\\programacion multimedia\\vlc-2.1.2\\";
+	
+	public EmbeddedMediaPlayerComponent mediaPlayer;
+	private JLabel lbVolumen;
+	public boolean tiempo = true;
+	public String dimension;
+	public Reproduccion reproduccion;
+	private ListaReproduccion listaReproduccion;
+	private Recientes reciente;
+	private JPanel panel;
+	private JMenuItem mntmAbrirStriming;
+	private JMenuItem mntmRetornarPantalla;
+	private FullScreenStrategy fullScreenStrategy;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Ventana window = new Ventana();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Ventana() {
+		initialize();
+		inicializar();
+		
+		frame.setTitle("HGM");
+	}
+	
+	private void inicializar(){
+		try {
+			PrintWriter recientes = new PrintWriter (new BufferedWriter(new FileWriter("Reciente", true)));
+			recientes.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		
+		
+		cargarReproductor();
+		reproduccion = new Reproduccion();
+		reproduccion.inicializar(this);
+		
+		listaReproduccion = new ListaReproduccion(this);
+		reciente = new Recientes(this);
+	}
+	
+	private void cargarReproductor(){
+		
+		NativeLibrary.addSearchPath(RuntimeUtil.getLibVlcLibraryName(), ruta_libreria);
+		Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(), LibVlc.class);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		mediaPlayer = new EmbeddedMediaPlayerComponent();
+		
+		this.panel.add(mediaPlayer);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setIconImage(Toolkit.getDefaultToolkit().getImage("pictograms-aem-0010-wear_ear_protection.png"));
+		frame.setMinimumSize(new Dimension(615, 425));
+		frame.setBounds(100, 100, 615, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
+		JPanel panelOpciones = new JPanel();
+		frame.getContentPane().add(panelOpciones, BorderLayout.SOUTH);
+		
+		btRetrasar = new JButton("");
+		btRetrasar.setEnabled(false);
+		btRetrasar.setBounds(0, 0, 25, 25);
+		btRetrasar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(mediaPlayer.getMediaPlayer().getTime() < 3000){
+					mediaPlayer.getMediaPlayer().setTime(0);
+				}
+				else{
+					mediaPlayer.getMediaPlayer().setTime(mediaPlayer.getMediaPlayer().getTime() - 3000);
+				}
+			}
+		});
+		btRetrasar.setIcon(new ImageIcon("media-seek-backward-7.png"));
+		panelOpciones.add(btRetrasar);
+		
+		btPlay = new JButton("");
+		btPlay.setEnabled(false);
+		btPlay.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				reproduccion.play();
+			}
+		});
+		btPlay.setIcon(new ImageIcon("media-playback-start-7.png"));
+		panelOpciones.add(btPlay);
+		
+		btPause = new JButton("");
+		btPause.setEnabled(false);
+		btPause.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				reproduccion.pausar();
+			}
+		});
+		btPause.setIcon(new ImageIcon("media-playback-pause-7.png"));
+		panelOpciones.add(btPause);
+		
+		btStop = new JButton("");
+		btStop.setEnabled(false);
+		btStop.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				reproduccion.stop();
+			}
+		});
+		btStop.setIcon(new ImageIcon("media-playback-stop-7.png"));
+		panelOpciones.add(btStop);
+		
+		btAdelantar = new JButton("");
+		btAdelantar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if(mediaPlayer.getMediaPlayer().getTime() > mediaPlayer.getMediaPlayer().getLength()-3000){
+					mediaPlayer.getMediaPlayer().setTime(mediaPlayer.getMediaPlayer().getLength());
+				}
+				else{
+					mediaPlayer.getMediaPlayer().setTime(mediaPlayer.getMediaPlayer().getTime() + 3000);
+				}
+			}
+		});
+		btAdelantar.setEnabled(false);
+		btAdelantar.setIcon(new ImageIcon("media-seek-forward-7.png"));
+		panelOpciones.add(btAdelantar);
+		
+		slVolumen = new JSlider();
+		slVolumen.setValue(100);
+		slVolumen.setMaximum(200);
+		slVolumen.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent evento) {
+				mediaPlayer.getMediaPlayer().setVolume(slVolumen.getValue());
+				lbVolumen.setText(Integer.toString(slVolumen.getValue()));
+			}
+		});
+		slVolumen.setEnabled(false);
+		panelOpciones.add(slVolumen);
+		
+		lbVolumen = new JLabel(Integer.toString(slVolumen.getValue()));
+		panelOpciones.add(lbVolumen);
+		
+		JPanel panelVentana = new JPanel();
+		frame.getContentPane().add(panelVentana, BorderLayout.CENTER);
+		panelVentana.setLayout(new BorderLayout(0, 0));
+		
+		slTiempo = new JSlider();
+		slTiempo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				mediaPlayer.getMediaPlayer().setTime(slTiempo.getValue());
+				tiempo = true;
+			}
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				tiempo = false;
+			}
+		});
+		slTiempo.setValue(0);
+		slTiempo.setEnabled(false);
+		panelVentana.add(slTiempo, BorderLayout.SOUTH);
+		
+		panel = new JPanel();
+		panelVentana.add(panel, BorderLayout.CENTER);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.getContentPane().add(menuBar, BorderLayout.NORTH);
+		
+		JMenu mnArchivo = new JMenu("Archivo");
+		menuBar.add(mnArchivo);
+		
+		mntmAbrir = new JMenuItem("Abrir archivo");
+		mntmAbrir.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				reproduccion.abrirArchivo();
+			}
+		});
+		mnArchivo.add(mntmAbrir);
+		
+		mntmAbrirDvd = new JMenuItem("Abrir DVDs");
+		mntmAbrirDvd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				String mrl = new SimpleDvdMrl().device("/D:\\").title(0).chapter(0).angle(0).value();
+				reproduccion.reproducirDVD(mrl);
+			}
+		});
+		mnArchivo.add(mntmAbrirDvd);
+		
+		mntmAbrirCds = new JMenuItem("Abrir CDs");
+		mntmAbrirCds.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				 String mrl = new CdMrl().device("/D:\\").track(0).value();
+				 reproduccion.reproducirDVD(mrl);
+			}
+		});
+		mnArchivo.add(mntmAbrirCds);
+		
+		mntmAbrirStriming = new JMenuItem("Abrir striming");
+		mntmAbrirStriming.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Striming ruta = new Striming();
+				ruta.mostrar(reproduccion);
+			}
+		});
+		mnArchivo.add(mntmAbrirStriming);
+		
+		JMenu mnReproductor = new JMenu("Reproductor");
+		menuBar.add(mnReproductor);
+		
+		mntmListaDeReproduccion = new JMenuItem("Lista de reproduccion");
+		mntmListaDeReproduccion.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				listaReproduccion.mostrar();
+			}
+		});
+		mnReproductor.add(mntmListaDeReproduccion);
+		
+		mntmRecientes = new JMenuItem("Recientes");
+		mntmRecientes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				reciente.mostrar();
+			}
+		});
+		mnReproductor.add(mntmRecientes);
+		
+		mntmCaptura = new JMenuItem("Captura");
+		mntmCaptura.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mediaPlayer.getMediaPlayer().saveSnapshot(new File(""));
+			}
+		});
+		mnReproductor.add(mntmCaptura);
+		
+		mntmSubtitulos = new JMenuItem("Subtitulos");
+		mntmSubtitulos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				reproduccion.subtitulos();
+			}
+		});
+		mnReproductor.add(mntmSubtitulos);
+		
+		JMenu mnAjustes = new JMenu("Tama\u00F1o");
+		menuBar.add(mnAjustes);
+		
+		mntmTamano = new JMenuItem("4 : 3");
+		mntmTamano.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				mediaPlayer.getMediaPlayer().setCropGeometry("4:3");
+			}
+		});
+		
+		mntmPredeterminada = new JMenuItem("Predeterminada");
+		mntmPredeterminada.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mediaPlayer.getMediaPlayer().setCropGeometry(dimension);
+			}
+		});
+		mnAjustes.add(mntmPredeterminada);
+		mnAjustes.add(mntmTamano);
+		
+		menuItem = new JMenuItem("16 : 9");
+		menuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mediaPlayer.getMediaPlayer().setCropGeometry("16:9");
+			}
+		});
+		mnAjustes.add(menuItem);
+		
+		menuItem_1 = new JMenuItem("16 : 10");
+		menuItem_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mediaPlayer.getMediaPlayer().setCropGeometry("16:10");
+			}
+		});
+		mnAjustes.add(menuItem_1);
+		
+		mntmPantallaCompleta = new JMenuItem("Pantalla Completa");
+		mntmPantallaCompleta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				fullScreenStrategy = new DefaultFullScreenStrategy(frame);
+				frame.setResizable(false);
+				fullScreenStrategy.enterFullScreenMode();
+				
+				mntmPantallaCompleta.setEnabled(false);
+				mntmRetornarPantalla.setEnabled(true);
+			}
+		});
+		mnAjustes.add(mntmPantallaCompleta);
+		
+		mntmRetornarPantalla = new JMenuItem("Retornar Pantalla");
+		mntmRetornarPantalla.setEnabled(false);
+		mntmRetornarPantalla.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				fullScreenStrategy.exitFullScreenMode();
+				frame.setResizable(true);
+				mntmPantallaCompleta.setEnabled(true);
+				mntmRetornarPantalla.setEnabled(false);
+			}
+		});
+		mnAjustes.add(mntmRetornarPantalla);
+		
+		frame.setLocationRelativeTo(null);
+	}
+
+	@Override
+	public void enterFullScreenMode() {
+		
+		
+	}
+
+	@Override
+	public void exitFullScreenMode() {
+		
+		
+	}
+
+	@Override
+	public boolean isFullScreenMode() {
+		
+		return false;
+	}
+	
+}
---
+++ ReproductorHector.2/src/org/hectordam/reproductorHector/beans/TablaReproduccion.java
@@ -0,0 +1,90 @@
+package org.hectordam.reproductorHector.beans;
+
+import java.awt.Button;
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import org.hectordam.reproductorHector.Ventana;
+import org.hectordam.reproductorHector.base.Reproduccion;
+import org.hectordam.reproductorHector.util.Filtro;
+
+public class TablaReproduccion extends JTable{
+
+	private DefaultTableModel modelo;
+	private ArrayList<String> lista;
+	
+	private JButton btEliminar;
+	private JButton btReproducir;
+	
+	public TablaReproduccion(JButton eliminar, JButton reproducir){
+		
+		String[] columna = {"lista"};
+		
+		modelo = new DefaultTableModel(columna, 0);
+		this.setModel(modelo);
+		
+		this.lista = new ArrayList<String>();
+		this.btReproducir = reproducir;
+		this.btEliminar = eliminar;
+	}
+	
+	public void listar(){
+		
+		modelo.setNumRows(0);
+
+		for(int i = 0; i < lista.size(); i++){
+			
+			String[] fila = {lista.get(i)};
+			
+			modelo.addRow(fila);
+		}
+		
+	}
+	
+	public void anadir(){
+		
+		JFileChooser fileChooser = new JFileChooser();
+		fileChooser.setFileFilter(new Filtro());
+		fileChooser.setAcceptAllFileFilterUsed(false);
+		if (fileChooser.showOpenDialog(null) == JFileChooser.CANCEL_OPTION){
+			return;
+		}
+		
+		lista.add(fileChooser.getSelectedFile().getAbsolutePath());
+		listar();
+		
+		btEliminar.setEnabled(true);
+		btReproducir.setEnabled(true);
+	}
+	
+	public void eliminar(){
+		
+		if(getSelectedRow() == -1){
+			return;
+		}
+		
+		lista.remove(getSelectedRow());
+		
+		listar();
+		
+		if(lista.size() == 0){
+			btEliminar.setEnabled(false);
+			btReproducir.setEnabled(false);
+		}
+		
+	}
+
+	public ArrayList<String> getLista() {
+		return lista;
+	}
+
+	public void setLista(ArrayList<String> lista) {
+		this.lista = lista;
+	}
+	
+}
